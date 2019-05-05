2b556a5d5c14e89ab6671b4351cea1eae4d09acf
Carlos Levir <carloslevirfs@gmail.com>
2019-05-05 14:16:37 -0300

    Eslint Config.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readable",
+        "SharedArrayBuffer": "readable"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
+};
