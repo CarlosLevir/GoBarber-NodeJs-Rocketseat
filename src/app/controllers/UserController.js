12aea3af84c26ef3ef5f1c0dfba9b6c1f36fd809
Carlos Levir <carloslevirfs@gmail.com>
2019-05-05 16:17:21 -0300

    Build of User Controller.

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,17 @@
+const { User } = require('../models');
+
+class UserController {
+  create(req, res) {
+    return res.render('auth/signup');
+  }
+
+  async store(req, res) {
+    const { filename: avatar } = req.file;
+
+    await User.create({ ...req.body, avatar });
+
+    return res.redirect('/');
+  }
+}
+
+module.exports = new UserController();
