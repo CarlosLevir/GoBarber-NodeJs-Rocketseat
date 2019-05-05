3df238e7302dfd29058763bba1ad19496268192d
Carlos Levir <carloslevirfs@gmail.com>
2019-05-05 14:22:44 -0300

    Sequelize Model Index Config.

---
+++ src/app/models/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const basename = path.basename(__filename);
+const config = require('../../config/database');
+
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
