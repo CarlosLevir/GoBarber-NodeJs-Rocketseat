cd280c725daf1ba7828e4636b8e035ce6a1cef14
Carlos Levir <carloslevirfs@gmail.com>
2019-05-05 16:16:55 -0300

    User Password Encryption.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,10 +1,19 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     avatar: DataTypes.STRING,
+    password: DataTypes.VIRTUAL,
     password_hash: DataTypes.STRING,
     provider: DataTypes.BOOLEAN,
+  }, {
+    hooks: {
+      beforeSave: async (user) => {
+        if (user.password) await bcrypt.hash(user.password, 10);
+      },
+    },
   });
 
   return User;
