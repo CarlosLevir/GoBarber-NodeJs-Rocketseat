c5a102cad4bebe29415cb3882663c04248ca0153
Carlos Levir <carloslevirfs@gmail.com>
2019-05-05 14:20:25 -0300

    Build of Users Migration.

---
+++ src/database/migrations/20190504144550-create-users.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: Sequelize.STRING,
+    },
+    avatar: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    password_hash: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    provider: {
+      allowNull: false,
+      defaultValue: false,
+      type: Sequelize.BOOLEAN,
+    },
+    created_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updated_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+
+  down: queryInterface => queryInterface.dropTable('users'),
+};
