9ccf31887a5db396b2e0a4ea7f39346a62c1fc1e
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-05-06 17:15:30 -0300

    Refactoring of Routes.

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,22 @@ const multer = require('multer');
 const multerConfig = require('./config/multer');
 
 const UserController = require('./app/controllers/UserController');
+const SessionController = require('./app/controllers/SessionController');
+
+const authMiddleware = require('./app/middlewares/auth');
+const guestMiddleware = require('./app/middlewares/guest');
 
 const routes = express.Router();
 
-routes.get('/signup', UserController.create);
+routes.get('/', guestMiddleware, SessionController.create);
+routes.post('/signin', SessionController.store);
+
+routes.get('/signup', guestMiddleware, UserController.create);
 routes.post('/signup', multer(multerConfig).single('avatar'), UserController.store);
+
+routes.use('/app', authMiddleware);
+
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'));
+routes.get('/app/logout', SessionController.destroy);
 
 module.exports = routes;
