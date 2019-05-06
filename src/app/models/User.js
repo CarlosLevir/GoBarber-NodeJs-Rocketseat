5c18fdf6a7bcae672ffd08f7336a9fd6157d2d87
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-05-06 17:16:27 -0300

    Build of Password Verification.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,20 +1,28 @@
 const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    password: DataTypes.VIRTUAL,
-    password_hash: DataTypes.STRING,
-    provider: DataTypes.BOOLEAN,
-  }, {
-    hooks: {
-      beforeSave: async (user) => {
-        if (user.password) await bcrypt.hash(user.password, 10);
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING,
+      provider: DataTypes.BOOLEAN,
+    },
+    {
+      hooks: {
+        beforeSave: async (user) => {
+          if (user.password) await bcrypt.hash(user.password, 10);
+        },
       },
     },
-  });
+  );
+
+  User.prototype.checkPassword = function checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  };
 
   return User;
 };
