fe5bc14ed13bacce413c6de357b1a055a1496a7f
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-05-08 08:25:10 -0300

    Completion and Refactoring of Routes.

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,26 @@ const express = require('express');
 const multer = require('multer');
 const multerConfig = require('./config/multer');
 
+const routes = express.Router();
+
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
+const DashboardController = require('./app/controllers/DashboardController');
+const FileController = require('./app/controllers/FileController');
+const AppointmentController = require('./app/controllers/AppointmentController');
+const AvailableController = require('./app/controllers/AvailableController');
 
 const authMiddleware = require('./app/middlewares/auth');
 const guestMiddleware = require('./app/middlewares/guest');
 
-const routes = express.Router();
+routes.use('/app', authMiddleware);
+
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success');
+  res.locals.flashError = req.flash('error');
+
+  return next();
+});
 
 routes.get('/', guestMiddleware, SessionController.create);
 routes.post('/signin', SessionController.store);
@@ -16,9 +29,14 @@ routes.post('/signin', SessionController.store);
 routes.get('/signup', guestMiddleware, UserController.create);
 routes.post('/signup', multer(multerConfig).single('avatar'), UserController.store);
 
-routes.use('/app', authMiddleware);
+routes.get('/app/files/:file', FileController.show);
 
-routes.get('/app/dashboard', (req, res) => res.render('dashboard'));
+routes.get('/app/dashboard', DashboardController.index);
 routes.get('/app/logout', SessionController.destroy);
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create);
+routes.post('/app/appointments/new/:provider', AppointmentController.store);
+
+routes.get('/app/available/:provider', AvailableController.index);
 
 module.exports = routes;
