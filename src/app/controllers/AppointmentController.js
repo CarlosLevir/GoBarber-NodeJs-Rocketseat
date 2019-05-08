d3d2fd03f16020232e4af816296ee12351c5bcf4
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-05-08 08:14:13 -0300

    Build of Appointment Store Method.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,24 @@
-const { User } = require('../models');
+const { User, Appointment } = require('../models');
 
 class AppointmentController {
   async create(req, res) {
     const provider = await User.findByPk(req.params.provider);
 
     return res.render('appointments/create', { provider });
+  }
+
+  async store(req, res) {
+    const { id: user_id } = req.session.user;
+    const { provider: provider_id } = req.params;
+    const { date } = req.body;
+
+    await Appointment.create({
+      user_id,
+      provider_id,
+      date,
+    });
+
+    return res.render('app/dashboard');
   }
 }
 
