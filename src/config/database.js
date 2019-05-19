082247dbb748b78b1ef0fe0115e3a8022e2a3f31
Carlos Levir <carloslevirfs@gmail.com>
2019-05-19 10:22:06 -0300

    Add of .ENV Fields.

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
-  host: '127.0.0.1',
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT || 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: 'GoBarber',
+  database: process.env.DB_NAME,
   operatorAliases: false,
   define: {
     timestamps: true,
